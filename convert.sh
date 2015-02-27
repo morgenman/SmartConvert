c1160f6048b9c67b289c6505cf45e2bcc9aa71bc
Cole Morgen <morgenman@gmail.com>
2015-02-27 13:28:06 +0100

    added script file

---
+++ convert.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script should take a smartboard notebook file, extract it to a temp dir, convert the svg's to png, and compile them into pdf
+# Depends on imagemagick, unzip
+mkdir ~/.temp/SmartConvert/
+
+
+
+rm -rf ~/.temp/SmartConvert/
